be529b80e5d0ee211c9aeb23d21c279686dbc4e1
Bryan Jonker <jonker@illinois.edu>
2025-12-15 08:37:53 -0600

    Update Controller

--- uofi-itp-directory/Controllers/CampusExportController.cs
+++ uofi-itp-directory/Controllers/CampusExportController.cs
@@ -17,7 +17,15 @@ namespace uofi_itp_directory.Controllers {
                 return NotFound();
             }
             dynamic? person = JsonConvert.DeserializeObject(jsonString);
-            return person == null || person?.username?.ToString() == "" ? NotFound() : View("CampusExportV1", person);
+            var show = false;
+            if (person != null || person?.username?.ToString() != "" && person?.jobProfiles != null) {
+                foreach (var profile in person?.jobProfiles) {
+                    if (profile.displayOrder > 0) {
+                        show = true;
+                    }
+                }
+            }
+            return person == null || person?.username?.ToString() == "" || !show ? NotFound() : View("CampusExportV1", person);
         }
     }
 }
