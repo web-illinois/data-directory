8c6ff40ebd7496243be0c19d2a1b6dd229387dba
Bryan Jonker <jonker@illinois.edu>
2026-07-10 08:31:55 -0500

    Clean URL string

--- uofi-itp-directory-search/SearchStax/SearchStaxLoader.cs
+++ uofi-itp-directory-search/SearchStax/SearchStaxLoader.cs
@@ -4,7 +4,7 @@ namespace uofi_itp_directory_search.SearchStax {
     public class SearchStaxLoader {
         public SearchStaxLoader(string? url, string? apiToken) {
             this.apiToken = apiToken ?? "";
-            this.url = url ?? "";
+            this.url = string.IsNullOrWhiteSpace(url) ? "" : url.TrimEnd('/');
         }
         private string apiToken;
         private string url;
@@ -15,21 +15,8 @@ namespace uofi_itp_directory_search.SearchStax {
                 return "SearchStax not set up";
             }
             using var client = new HttpClient();
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, url + "?commit=true");
+            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, url + "/update?commit=true");
             requestMessage.Content = new StringContent(s, Encoding.UTF8, "application/json");
-            requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", apiToken);
-            var response = await client.SendAsync(requestMessage);
-            return await response.Content?.ReadAsStringAsync() ?? "";
-        }
-
-        public async Task<string> SendDelete(string url) {
-            if (!IsValid) {
-                return "SearchStax not set up";
-            }
-            var json = "";
-            using var client = new HttpClient();
-            using var requestMessage = new HttpRequestMessage(HttpMethod.Post, url + "?commit=true");
-            requestMessage.Content = new StringContent(json, Encoding.UTF8, "application/json");
             requestMessage.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", apiToken);
             var response = await client.SendAsync(requestMessage);
             return await response.Content?.ReadAsStringAsync() ?? "";
